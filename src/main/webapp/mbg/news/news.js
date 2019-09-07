97ea2b1a0c50e472f72dab77c95d57e49327d70e
LJJ <liujianjian@booksroo.com>
2019-09-07 15:49:37 +0800

    dbg

--- src/main/webapp/mbg/news/news.js
+++ src/main/webapp/mbg/news/news.js
@@ -15,28 +15,33 @@ function initTable() {
         columns: [{
             title: '',
             field: 'title',
-            width: 550
-        },{
+            width: 550,
+            formatter: 'operateFormat'
+        }, {
             title: '',
             field: 'createTime',
             width: 170,
-            formatter: 'timeFormat',
+            formatter: 'timeFormat'
         }]
     });
 }
 
 function operateFormat(value, row, index) {
-    var operate = '<a onclick="detail(' + row.id + ')"><i class="glyphicon glyphicon-fullscreen"></i></a>&nbsp;&nbsp;&nbsp;';
-    operate += '<a onclick="edit(' + row.id + ')"><i class="glyphicon glyphicon-edit"></i></a>&nbsp;&nbsp;&nbsp;'
-    operate += '<a onclick="del(' + row.id + ')"><i class="glyphicon glyphicon-remove"></i></a>'
-    return operate;
+    // return value;
+    return '<a style="cursor:pointer;" onclick="detail(&apos;' + row.id + '&apos;)">' + value + '</a>';
 }
 
-function responseHandler(res) {
-    $.each(res.rows, function (i, row) {
-        // row.operate = '<a onclick="detail('+row+')"><i class="glyphicon glyphicon-fullscreen"></i></a>&nbsp;&nbsp;&nbsp;';
-        // row.operate += '<a onclick="edit('+row+')"><i class="glyphicon glyphicon-edit"></i></a>&nbsp;&nbsp;&nbsp;'
-        // row.operate += '<a onclick="del('+row.id+')"><i class="glyphicon glyphicon-remove"></i></a>'
-    });
-    return res;
+function detail(id) {
+    var row = $table.bootstrapTable('getRowByUniqueId', id);
+    console.info(row);
+    var html = row.coverImgUrl != '' ? '<img width="240" height="160" src="' + row.coverImgUrl + '" />' : '';
+    html += '<h4 style="font-size: 18px;font-weight: bold;line-height: 1.5;">标题：' + row.title + '</h4>';
+    var s = row.source != null ? row.source.length > 0 ? row.source : '-' : '-';
+    // html += '<h6><p>来源：' + s + '</p></h6>';
+    html += '<span class="font-12">创建时间：' + getLocalTime(row.createTime, 4) + '</span>';
+    // html += '<br/><span class="font-12">置顶：' + booleanCommonFormat(row.topFlag) + '&nbsp;&nbsp;显示：' + booleanCommonFormat(row.showFlag) + '</span>';
+    html += '<br/><br/>';
+    html += row.content;
+    $('#newsDetail').show();
+    $('#newsDetail>div>section').html(html);
 }
